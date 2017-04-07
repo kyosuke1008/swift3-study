4eec53480d467e614f3be3ff232d469612b1f673
石津恭輔 <kyosuke.ishizu@f.flect.co.jp>
2017-04-08 01:42:16 +0900

    Initial Commit

---
+++ study/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  study
+//
+//  Created by 石津恭輔 on 2017/04/08.
+//  Copyright © 2017年 石津恭輔. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
